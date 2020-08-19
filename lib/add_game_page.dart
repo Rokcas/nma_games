67025c4e4f60972323b17d43861c54415cacc3c3
Augustinas Jučas <augustinasjucas@gmail.com>
2020-08-19 13:51:47 +0300

    Added add_game_page.dart file

--- lib/add_game_page.dart
+++ lib/add_game_page.dart
@@ -1,19 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:nma_games/widgets/text_field_widget.dart';
 
 class AddGamePage extends StatelessWidget {
+
+
   @override
   Widget build(BuildContext context) {
+    var pav = TextFieldWidget(value: "Pavadinimas");
+    var apr = TextFieldWidget(value: "Aprašymas");
+    var own = TextFieldWidget(value: "Aprašymas");
     return Scaffold(
       appBar: AppBar(
         title: Text("Naujo žaidimo registracija"),
       ),
-      body: Center(
-        child: Column(
-          children:[
-            Container(child: Image.asset("assets/placeholder.jpg")),
+      body: Column(
+          children:
+          [
+            Center(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Container(child: Image.asset("assets/placeholder.jpg")),
+                  ),
+                  Center(child: pav),
+                  Center(child: apr)
 
+                ],
+              ),
+            ),
+            RaisedButton(
+//              tooltip: "Registruoti",
+              child: Text("Registruoti"),
+              color:Colors.blueAccent,
+              onPressed: (){
+                return showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      // Retrieve the text the user has entered by using the
+                      // TextEditingController.
+                      content: Text(pav.getVal()),
+                    );
+                  },
+                );
+              },
+            ),
           ]
-        ),
       ),
     );
   }
--- lib/widgets/text_field_widget.dart
+++ lib/widgets/text_field_widget.dart
@@ -1,19 +1,67 @@
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
 class TextFieldWidget extends StatefulWidget {
+
+  final myController = TextEditingController();
+
+//  @override
+//  void dispose() {
+//     Clean up the controller when the widget is disposed.
+//    myController.dispose();
+//    super.dispose();
+//  }
+
+
+  final String value ;
+  TextFieldWidget({Key key, this.value});
+
   @override
-  _TextFieldWidgetState createState() => _TextFieldWidgetState();
+  _TextFieldWidgetState createState() => _TextFieldWidgetState(value:value, myController:myController);
+
+  String getVal(){
+    print("esu cia");
+    print(myController.text);
+    return myController.text;
+  }
 }
 
 class _TextFieldWidgetState extends State<TextFieldWidget> {
+  String value;
+  final myController;
+  _TextFieldWidgetState({Key key, this.value, this.myController});
+
   @override
   Widget build(BuildContext context) {
+
     return Row(
       children: [
+        Container(
+          width: 200,
+          child: TextField(
+            decoration: InputDecoration(hintText: value),
+            controller: myController,
+//          selectionWidthStyle: BoxWidthStyle.max,
+          ),
+        ),
+        FloatingActionButton(
+          onPressed: () {
+            return showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  content: Text(myController.text),
+                );
+              },
+            );
+          },
+          tooltip: 'Show me the value!',
+          child: Icon(Icons.text_fields),
+        )
 
       ],
+      mainAxisAlignment: MainAxisAlignment.center,
     );
-
   }
 }
