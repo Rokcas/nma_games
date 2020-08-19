81258bb581e49f54aada631706c2e7697851a6e9
Rokcas <rokas0090@gmail.com>
2020-08-19 12:54:58 +0300

    Improve the style of the grid

--- lib/widgets/game_grid.dart
+++ lib/widgets/game_grid.dart
@@ -8,9 +8,36 @@ class GameGrid extends StatelessWidget {
         // Generate 100 widgets that display their index in the List.
         children: List.generate(100, (index) {
           return Center(
-            child: Text(
-              'Item $index',
-              style: Theme.of(context).textTheme.headline5,
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(16),
+                child: InkWell(
+                  onTap: () => print("TODO"),
+                  child: Stack(
+                    children: [
+                      Image.asset("assets/unspecified_game_photo.png"),
+                      Align(
+                        alignment: Alignment.bottomCenter,
+                        child: SizedBox(
+                          width: double.infinity,
+                          child: Container(
+                            color: Colors.black.withOpacity(0.5),
+                            child: Padding(
+                              padding: const EdgeInsets.all(2.0),
+                              child: Text(
+                                'Item $index',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ),
           );
         }));
