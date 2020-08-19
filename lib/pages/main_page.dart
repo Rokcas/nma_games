b5adaf319a906b0fe77f2ed13fd8ee2d35b4923e
Rokcas <rokas0090@gmail.com>
2020-08-19 11:35:26 +0300

    Create a navigation bar for the main page

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -7,12 +7,13 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   int _currentIndex = 0;
+  final tabNames = ["Todo 1", "Todo 2"];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("TODO"),
+        title: Text("${tabNames[_currentIndex]}"),
       ),
       body: Center(
         child: Column(
@@ -28,6 +29,20 @@ class _MainPageState extends State<MainPage> {
         onPressed: () => print("TODO"),
         tooltip: 'Increment',
         child: Icon(Icons.add),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTabTapped,
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.explore),
+            title: Text("${tabNames[0]}".toUpperCase()),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.movie),
+            title: Text("${tabNames[1]}".toUpperCase()),
+          ),
+        ],
       ),
     );
   }
